d55f1bba4b3fe84526f5cd57b74aa216bf49bf43
Suraj <thesurajsite@gmail.com>
2024-03-09 12:44:32 +0530

    first commit

---
+++ app/src/main/java/Activities/MainActivity.kt
@@ -0,0 +1,14 @@
+package Activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weatherwise.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        startActivity(Intent(this, SplashActivity::class.java))
+    }
+}
---
+++ app/src/main/java/Activities/SplashActivity.kt
@@ -0,0 +1,20 @@
+package Activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.weatherwise.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },2000)
+    }
+}

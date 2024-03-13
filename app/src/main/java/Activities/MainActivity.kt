9551a09fbc23166f3d3a53bf1a7002e5329ae261
Suraj <thesurajsite@gmail.com>
2024-03-13 13:37:33 +0530

    location feature completed

--- app/src/main/java/Activities/MainActivity.kt
+++ app/src/main/java/Activities/MainActivity.kt
@@ -1,7 +1,6 @@
 package Activities
 
 import android.Manifest
-import android.app.DownloadManager.Query
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
@@ -14,11 +13,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Vibrator
 import android.provider.Settings
-import android.renderscript.ScriptGroup.Binding
 import android.util.Log
 import android.view.View
 import android.widget.SearchView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.weatherwise.R
@@ -29,15 +26,11 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.sql.Timestamp
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
-import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import java.util.Objects
-import java.util.concurrent.locks.Condition
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -273,7 +266,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             "Haze","Partly Clouds","Clouds","Overcast","Mist", "Foggy" -> {
-                binding.root.setBackgroundResource(R.drawable.colud_background)
+                binding.root.setBackgroundResource(R.drawable.cloud_background)
                 binding.lottieAnimationView.setAnimation(R.raw.cloud)
 
             }
